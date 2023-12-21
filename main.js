e17bf88d225baa8d199614b3d1a169c9929562d2
Xu-adam <154505958+Xu-adam@users.noreply.github.com>
2023-12-21 19:17:06 +0800

    Update main.js

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ alert("Welcome to my test page!")
 let myImage = document.querySelector("img");
 myImage.onclick = function() {
   let mySrc = myImage.getAttribute("src");
-  if (mySrc === "image-1.webp") {
-    myImage.setAttribute("src", "image-2.webp");
+  if (mySrc === "images/image-1.webp") {
+    myImage.setAttribute("src", "images/image-2.webp");
   } else {
-    myImage.setAttribute("src", "image-1.webp");
+    myImage.setAttribute("src", "images/image-1.webp");
   }
 };
 
@@ -14,11 +14,19 @@ myImage.onclick = function() {
 let myButton = document.querySelector("button");  //button press
 let myHeading = document.querySelector("h1");  //welcome heading
 
-setUserName();
 function setUserName() {
     let myName = prompt("Please enter your name here");
-    myHeading.textContent = "Welcome " + myName; 
+    if (myName){
+      myHeading.textContent = "Welcome " + myName;
+    }
+    else{
+      setUserName();
+    }
 }
+
+setUserName();
+
+
 
 myButton.onclick = function () {
     setUserName();
